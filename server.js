4aaec9e24f6c523113fccc991f7cb462bc5709ec
F.joker <35176464+nothing0is0impossible@users.noreply.github.com>
2018-03-07 20:44:18 +0700

    them server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const server = new express();
+
+const port = 9000;
+
+server.use(express.static("public"))
+
+server.get("/api/posts", (req, res) => {
+    const posts = [
+        { title: "Mot so luu y" },
+        { title: "Hello" },
+        { title: "goodbyw" },
+        { title: "nihao" }
+    ];
+
+    res.json({ success: true, posts });
+})
+
+server.get("/", (req, res) => {
+    res.sendfile(__dirname + "/public/thanggit.html")
+})
+
+server.listen(port, (req, res) => {
+    console.log("Server dang chay o cong 9000");
+});
